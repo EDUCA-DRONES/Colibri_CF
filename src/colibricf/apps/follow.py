9958e90acd7aa0ccefcca24ff5da7c616af1f6b0
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-11-11 16:24:37 -0300

    fix some bugs

--- src/colibricf/apps/follow.py
+++ src/colibricf/apps/follow.py
@@ -58,8 +58,8 @@ def _follow_callback(data):
 
         if target_size is not None:
             landmark0 = detection_results.pose_landmarks[0][0]
-            Drone().centralize_in_target(center, landmark0, w)
-            Drone().follow_handle_move(target_size)
+            centralize_in_target(center, landmark0, w)
+            follow_handle_move(target_size)
 
         annotated_frame= draw_landmarks_on_image(frame, detection_results)
         frame = cv.cvtColor(annotated_frame, cv.COLOR_RGB2BGR)
