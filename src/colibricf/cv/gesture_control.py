7df0b1bb098e014f54845382c293ac19b4abc448
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-11-11 15:22:37 -0300

    fix: initializing mp_hands outside of _gc_callback

--- src/colibricf/cv/gesture_control.py
+++ src/colibricf/cv/gesture_control.py
@@ -32,37 +32,38 @@ def handle_fingercount(points):
 
     return count
 
+hands = mp_hands.Hands(max_num_hands=2, model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5)
+
 @long_callback
 def _gc_callback(data):
-    with mp_hands.Hands(max_num_hands=2, model_complexity=0, min_detection_confidence=0.5, min_tracking_confidence=0.5) as hands:
-        frame = bridge.imgmsg_to_cv2(data, 'bgr8')
-        blank = np.zeros((500, 500, 3), dtype="uint8")
+    frame = bridge.imgmsg_to_cv2(data, 'bgr8')
+    blank = np.zeros((500, 500, 3), dtype="uint8")
 
-        frame = cv.flip(frame, 1)
-        frame.flags.writeable = False
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        results = hands.process(frame)
-        frame.flags.writeable = True
-        frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
+    frame = cv.flip(frame, 1)
+    frame.flags.writeable = False
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+    results = hands.process(frame)
+    frame.flags.writeable = True
+    frame = cv.cvtColor(frame, cv.COLOR_RGB2BGR)
 
-        h,w,_ = frame.shape
+    h,w,_ = frame.shape
 
-        points = []
-        dedos = [8, 12, 16, 20]
-        if results.multi_hand_landmarks:
-            for hand_landmark in results.multi_hand_landmarks:
-                mp_drawing.draw_landmarks(blank, hand_landmark, mp_hands.HAND_CONNECTIONS, mp_styles.get_default_hand_landmarks_style(), mp_styles.get_default_hand_connections_style())
+    points = []
+    dedos = [8, 12, 16, 20]
+    if results.multi_hand_landmarks:
+        for hand_landmark in results.multi_hand_landmarks:
+            mp_drawing.draw_landmarks(blank, hand_landmark, mp_hands.HAND_CONNECTIONS, mp_styles.get_default_hand_landmarks_style(), mp_styles.get_default_hand_connections_style())
 
-                for index, cord in enumerate(hand_landmark.landmark):
-                    cx, cy = int(cord.x * w), int(cord.y * h)
-                    points.append((cx, cy))
+            for index, cord in enumerate(hand_landmark.landmark):
+                cx, cy = int(cord.x * w), int(cord.y * h)
+                points.append((cx, cy))
 
-                count = 0
-                if hand_landmark:
-                    count = handle_fingercount(points)
+            count = 0
+            if hand_landmark:
+                count = handle_fingercount(points)
 
-                cv.putText(blank, str(count), (20, 70), cv.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+            cv.putText(blank, str(count), (20, 70), cv.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
-                drone.gc_handle_move(count)
+            drone.gc_handle_move(count)
 
-        image_pub.publish(bridge.cv2_to_imgmsg(frame, 'bgr8'))
+    image_pub.publish(bridge.cv2_to_imgmsg(frame, 'bgr8'))
