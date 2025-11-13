843e178596da979742f1ad4513b10e9d4ef58e3b
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-11-12 21:30:47 -0300

    fixing record

---
+++ examples/record.py
@@ -0,0 +1,28 @@
+# Information: https://clover.coex.tech/programming
+# Use this example to create new missions
+
+import rospy
+from colibricf.task import Task
+
+class Mission(Task):
+    '''
+    Example of implementation.
+    '''
+
+    TAKEOFF_ALTITUDE = 1.4
+
+    def mission(self):
+        # Implement your mission here
+        self.camera.record()
+
+        print('note: arming')
+        self.drone.arm()
+        rospy.sleep(2)
+
+        print('Take off and hover 1.4 m above the ground')
+        self.drone.navigate_wait(x=0, y=0, z=self.TAKEOFF_ALTITUDE, frame_id='body', auto_arm=True)
+
+        self.camera.stop()
+
+Mission().run()
+
