19406e2ef125c70c48a6b7c6261c1b53d60993b0
Bruno Rodrigues <brunorodriguesmtv0@gmail.com>
2025-11-14 18:29:31 -0300

    add stop logger method

--- src/colibricf/files/logger.py
+++ src/colibricf/files/logger.py
@@ -2,7 +2,7 @@ import rospy
 from .filemanager import FileManager, Extension
 from rosgraph_msgs.msg import Log
 
-class TaskLogger:
+class Logger:
     def __init__(self):
         self.filemanager = FileManager()
         self.filename = self.filemanager.filename(Extension.TASK_LOG)
@@ -20,12 +20,20 @@ class TaskLogger:
             level = level_map.get(msg.level, 'UNKNOWN')
             timestamp = msg.header.stamp.to_sec()
 
-            line = f'[{level}] [{timestamp:.3f}]: {msg.msg}\n'
+            line = f'[{level}] [{timestamp:.5f}]: {msg.msg}\n'
             self.log_file.write(line)
             self.log_file.flush()
 
         self.log_file = open(self.filename, 'a')
-        rospy.Subscriber('/rosout', Log, _callback)
+        self.rosout = rospy.Subscriber('/rosout', Log, _callback)
         rospy.loginfo(f'Task log started: {self.filename}')
 
+    def stop(self):
+        try:
+            if self.file and self.rosout is not None:
+                self.rosout.unregister()
+                self.log_file.flush()
+                self.log_file.close()
 
+        except Exception as e:
+            rospy.logerr()
